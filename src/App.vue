910c0c017b61fb7b1d672fa0777a979280cebb3d
Antonio Jurić <juricantonio18@gmail.com>
2024-08-03 17:45:33 +0200

    Created task list component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
 import AddTask from "@/components/AddTask.vue";
 import FilterTask from "@/components/FilterTask.vue";
-
-import { ref } from "vue";
-import { useTodoStore } from "./store/todo";
-import { storeToRefs } from "pinia";
-
-const todoStore = useTodoStore();
-const { filteredTodo, showFinished } = storeToRefs(todoStore);
-const { addTodo, toggleTodo, toggleFilter } = todoStore;
+import TaskList from "@/components/TaskList.vue";
 </script>
 
 <template id="app">
@@ -22,21 +15,7 @@ const { addTodo, toggleTodo, toggleFilter } = todoStore;
 
       <AddTask />
       <FilterTask />
-
-      <div class="mt-10 flex flex-col gap-2 w-full">
-        <div
-          v-for="t in filteredTodo"
-          class="bg-emerald-900 text-white py-2 px-2 pl-4 rounded-lg shadow-lg flex justify-between items-center"
-        >
-          <div :class="t.isFinished && 'line-through'">{{ t.text }}</div>
-          <button
-            class="bg-emerald-950 py-1 px-4 rounded-lg shadow-lg"
-            @click="() => toggleTodo(t.id)"
-          >
-            {{ !t.isFinished ? "Do" : "Undo" }}
-          </button>
-        </div>
-      </div>
+      <TaskList />
     </section>
   </div>
 </template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useTodoStore } from "@/store/todo";
+import { storeToRefs } from "pinia";
+
+const todoStore = useTodoStore();
+const { filteredTodo } = storeToRefs(todoStore);
+const { addTodo, toggleTodo } = todoStore;
+</script>
+
+<template>
+  <div class="mt-10 flex flex-col gap-2 w-full">
+    <div
+      v-for="t in filteredTodo"
+      class="bg-emerald-900 text-white py-2 px-2 pl-4 rounded-lg shadow-lg flex justify-between items-center"
+    >
+      <div :class="t.isFinished && 'line-through'">{{ t.text }}</div>
+      <button
+        class="bg-emerald-950 py-1 px-4 rounded-lg shadow-lg"
+        @click="() => toggleTodo(t.id)"
+      >
+        {{ !t.isFinished ? "Do" : "Undo" }}
+      </button>
+    </div>
+  </div>
+</template>
