e54f65f2b19151c88d06522b891b9e2856bc9781
Antonio Jurić <juricantonio18@gmail.com>
2024-08-03 18:18:17 +0200

    Added saving and fetching from local storage

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
 import AddTask from "@/components/AddTask.vue";
 import FilterTask from "@/components/FilterTask.vue";
 import TaskList from "@/components/TaskList.vue";
+
+import { onMounted } from "vue";
+import { useTodoStore } from "./store/todo";
+
+const todoStore = useTodoStore();
+const { initializeStore } = todoStore;
+
+onMounted(() => {
+  initializeStore();
+});
 </script>
 
 <template id="app">
@@ -10,7 +20,9 @@ import TaskList from "@/components/TaskList.vue";
       class="mt-20 flex flex-col gap-5 justify-center items-center w-[80%] max-w-[350px]"
     >
       <div class="text-3xl font-thin mb-10">
-        <span class="bg-emerald-500 rounded-lg px-2 py-1 mr-1 font-thin">Task</span>List
+        <span class="bg-emerald-500 rounded-lg px-2 py-1 mr-1 font-thin"
+          >Task</span
+        >List
       </div>
 
       <AddTask />
--- src/store/todo.js
+++ src/store/todo.js
@@ -14,6 +14,13 @@ export const useTodoStore = defineStore("todo", {
     },
   },
   actions: {
+    initializeStore() {
+      const storedTodo = localStorage.getItem("todo");
+      const storedFilter = localStorage.getItem("showFinished");
+
+      if (storedTodo) this.todo = JSON.parse(storedTodo);
+      if (storedFilter) this.showFinished = JSON.parse(storedFilter);
+    },
     addTodo(text) {
       if (!text) return;
       this.todo.unshift({
@@ -21,16 +28,24 @@ export const useTodoStore = defineStore("todo", {
         text: text,
         isFinished: false,
       });
+      this.updateStorage();
     },
     removeTodo(id) {
       this.todo = this.todo.filter((t) => t.id !== id);
+      this.updateStorage();
     },
     toggleTodo(id) {
       const index = this.todo.findIndex((todo) => todo.id === id);
       this.todo[index].isFinished = !this.todo[index].isFinished;
+      this.updateStorage();
     },
     toggleFilter(newValue) {
       this.showFinished = newValue;
+      this.updateStorage();
+    },
+    updateStorage() {
+      localStorage.setItem("todo", JSON.stringify(this.todo));
+      localStorage.setItem("showFinished", JSON.stringify(this.showFinished));
     },
   },
 });
