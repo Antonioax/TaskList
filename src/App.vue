c342566a504c4d74d95fe26a54a9694aab5fdc72
Antonio Jurić <juricantonio18@gmail.com>
2024-08-03 17:07:28 +0200

    Updated template

--- src/App.vue
+++ src/App.vue
@@ -12,33 +12,46 @@ const { addTodo, toggleTodo } = todoStore;
 const addNewTodo = () => {
   addTodo(newTodo.value);
   newTodo.value = "";
-}
+};
 </script>
 
 <template id="app">
-  <section class="mt-20 flex flex-col gap-5 justify-center items-center">
-    <input
-      type="text"
-      class="text-center py-2 rounded-xl shadow-xl w-[80%] max-w-[350px]"
-      v-model="newTodo"
-    />
-    <button
-      class="bg-emerald-100 py-1 px-4 rounded-lg shadow-lg"
-      @click="addNewTodo"
+  <div class="flex justify-center">
+    <section
+      class="mt-20 flex flex-col gap-5 justify-center items-center w-[80%] max-w-[350px]"
     >
-      Add Task
-    </button>
-
-    <div class="mt-10 flex flex-col gap-2 w-[80%] max-w-[350px]">
-      <div
-        v-for="t in todo"
-        class="bg-emerald-900 text-white py-2 px-2 pl-4 rounded-lg shadow-lg flex justify-between items-center"
-      >
-        <div :class="t.isFinished && 'line-through'">{{ t.text }}</div>
-        <button class="bg-emerald-950 py-1 px-4 rounded-lg shadow-lg" @click="() => toggleTodo(t.id)">
-          {{ !t.isFinished ? "Do" : "Undo" }}
-        </button>
+      <div class="text-4xl font-thin mb-10">
+        <span class="bg-emerald-500 rounded-lg px-2 mr-1">Task</span>List
       </div>
-    </div>
-  </section>
+      <input
+        type="text"
+        class="text-center py-2 rounded-xl shadow-xl w-full"
+        v-model="newTodo"
+      />
+      <button
+        class="bg-emerald-100 py-1 px-4 rounded-lg shadow-lg"
+        @click="addNewTodo"
+      >
+        Add Task
+      </button>
+      <div class="flex justify-start items-center w-full ml-4 mt-4">
+          <input type="checkbox" name="unfinished" id="unfinished" />
+          <label for="unfinished" class="text-xs ml-4">SHOW FINISHED TASKS</label>
+      </div>
+      <div class="mt-10 flex flex-col gap-2 w-full">
+        <div
+          v-for="t in todo"
+          class="bg-emerald-900 text-white py-2 px-2 pl-4 rounded-lg shadow-lg flex justify-between items-center"
+        >
+          <div :class="t.isFinished && 'line-through'">{{ t.text }}</div>
+          <button
+            class="bg-emerald-950 py-1 px-4 rounded-lg shadow-lg"
+            @click="() => toggleTodo(t.id)"
+          >
+            {{ !t.isFinished ? "Do" : "Undo" }}
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
