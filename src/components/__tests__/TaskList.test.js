5d4c9e1eaef02f34e6ef523e72d2cb30f252db26
Antonio Jurić <juricantonio18@gmail.com>
2024-08-05 22:14:57 +0200

    Added new tests

--- src/components/__tests__/TaskList.test.js
+++ src/components/__tests__/TaskList.test.js
@@ -4,12 +4,14 @@ import { createTestingPinia } from "@pinia/testing";
 
 import { useTodoStore } from "@/store/todo";
 import TaskList from "../TaskList.vue";
+import { setActivePinia } from "pinia";
 
 describe("TaskList.vue", () => {
   let wrapper;
+  let pinia;
 
   beforeEach(() => {
-    const pinia = createTestingPinia({
+    pinia = createTestingPinia({
       createSpy: vi.fn,
       initialState: {
         todo: {
@@ -41,11 +43,25 @@ describe("TaskList.vue", () => {
       },
     });
 
-    wrapper = mount(TaskList, { plugins: [pinia] });
+    setActivePinia(pinia);
+
+    wrapper = mount(TaskList, { global: { plugins: [pinia] } });
   });
 
-  it("should render tasks when filteredTodo has items", () => {
+  it("should render tasks when filteredTodo has items", async () => {
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.text()).toContain("Task 1");
     expect(wrapper.text()).toContain("Task 2");
+  });
+
+  it("should not render finished tasks when showFinished === false", async () => {
+    const store = useTodoStore();
+    store.showFinished = false;
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.text()).toContain("Task 1");
+    expect(wrapper.text()).not.toContain("Task 2");
   });
 });
