6107213ee47cabecb4348950ca17c844fc8f2bfc
Antonio Jurić <juricantonio18@gmail.com>
2024-08-08 18:34:52 +0200

    Added home view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import AddTask from "@/components/AddTask.vue";
-import FilterTask from "@/components/FilterTask.vue";
-import TaskList from "@/components/TaskList.vue";
+import HomeView from "./views/HomeView.vue";
 
 import { onMounted } from "vue";
 import { useTodoStore } from "./store/todo";
@@ -15,19 +13,5 @@ onMounted(() => {
 </script>
 
 <template id="app">
-  <div class="flex justify-center">
-    <section
-      class="mt-20 flex flex-col gap-5 justify-center items-center w-[80%] max-w-[350px]"
-    >
-      <div class="text-3xl font-thin mb-10">
-        <span class="bg-emerald-500 rounded-lg px-2 py-1 mr-1 font-thin"
-          >Task</span
-        >List
-      </div>
-
-      <AddTask />
-      <FilterTask />
-      <TaskList />
-    </section>
-  </div>
+  <HomeView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AddTask from "@/components/AddTask.vue";
+import FilterTask from "@/components/FilterTask.vue";
+import TaskList from "@/components/TaskList.vue";
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <section
+      class="mt-20 flex flex-col gap-5 justify-center items-center w-[80%] max-w-[350px]"
+    >
+      <div class="text-3xl font-thin mb-10">
+        <span class="bg-emerald-500 rounded-lg px-2 py-1 mr-1 font-thin"
+          >Task</span
+        >List
+      </div>
+
+      <AddTask />
+      <FilterTask />
+      <TaskList />
+    </section>
+  </div>
+</template>
